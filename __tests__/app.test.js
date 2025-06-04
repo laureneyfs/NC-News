6b95701b767b20c4ec06e61d2591f7a4a0a705b3
laureneyfs <laureneyfs@gmail.com>
2025-06-04 11:22:32 +0100

    added /api endpoint that returns endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,17 @@
 const endpointsJson = require("../endpoints.json");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const request = require("supertest");
+const data = require("../db/data/test-data/index");
+const app = require("../app");
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
+beforeAll(() => seed(data));
+afterAll(() => db.end());
 
 describe("GET /api", () => {
-  test.skip("200: Responds with an object detailing the documentation for each endpoint", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
