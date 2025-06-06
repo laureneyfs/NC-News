5b7171bb6375607fe146ccf005a38072d4600d45
laureneyfs <laureneyfs@gmail.com>
2025-06-06 16:07:06 +0100

    adjusted connection file for production environment support

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,21 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+const config = {};
 
-const db = new Pool();
-
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("No PGDATABASE configured");
+} else {
+  console.log(`Connected to ${process.env.PGDATABASE}`);
+}
+const db = new Pool(config);
 
 module.exports = db;
